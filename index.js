276015a9d9541c37eec6dfd7fe6446344f660e54
Shiv9639 <passawalashivang@gmail.com>
2022-05-05 15:26:02 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
+const app = express();
+app.use(bodyParser.json({ extended: true }));
+app.use(cors());
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+app.get("/api", (req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
+    res.json({"users": ["UserOne","usertwo","userthree"]})
+})
 
+app.listen(5000, ()=> {console.log("Server started on port 5000")})
